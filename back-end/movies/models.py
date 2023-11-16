63dbc2d01a0947df8ac1c5442e72a39812b652af
Jiyong Kim <aop8021@naver.com>
2023-11-16 17:26:26 +0900

    111623_model_yet

--- back-end/movies/models.py
+++ back-end/movies/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.conf import settings
+import datetime
 
-# Create your models here.
+
+class Actor(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    profile_path = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
+
+class Director(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    profile_path = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    genres = models.ManyToManyField(Genre, related_name='movies')
+    actors = models.ManyToManyField(Actor, related_name='movies')
+    title = models.CharField(max_length=100)
+    original_language = models.CharField(max_length=20)
+    overview = models.TextField()
+    release_date = models.DateField(null=True, default=datetime.date.today)
+    runtime = models.IntegerField(null=True)
+    
+    poster_path = models.TextField(null=True)
+    poster_path = models.TextField(null=True)
+    
+    budget = models.BigIntegerField()
+    popularity = models.FloatField()
+    tagline = models.TextField(null=True)
+    vote_average = models.FloatField(null=True)
+    vote_count = models.IntegerField(null=True)
+    words = models.TextField(null=True)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='like_movies'
+    )
+
+    def __str__(self):
+        return self.title
