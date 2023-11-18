6d4b843882e4adbb529b8caa3609ac852ebd62ae
뚱이 <djjang0925@gmail.com>
2023-11-18 18:42:51 +0900

    movie_search(back-end)

--- back-end/movies/urls.py
+++ back-end/movies/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'movies'
 urlpatterns = [
-    path('test/', views.test)
+    # path('test/', views.test)
+    path('movie_search/<str:movie_title>/', views.movie_search)
 ]
