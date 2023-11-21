3a0e0c57e4fcca3b8abeb63c55ed175aec19da93
지용 <aop8021@naver.com>
2023-11-22 00:03:42 +0900

    poster_img_slideing_ing

--- front-end/movie-nomad/src/router/index.js
+++ front-end/movie-nomad/src/router/index.js
@@ -15,11 +15,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/test',
-      name: 'test',
-      component: () => import('@/views/TestView.vue')
-    },
-    {
       path: '/',
       name: 'home',
       component: HomeView
